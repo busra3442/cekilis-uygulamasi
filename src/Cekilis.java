a66bc728e16ea58c9d15938238f503479b5ad5cd
busrasevindik <busrasevindik182@gmail.com>
2025-03-24 00:01:54 +0300

    ilk hal

---
+++ src/Cekilis.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Cekilis {
+
+    private JPanel panel1;
+    private JTextField aramaCubugu;
+    private JButton Gözat;
+    private JList kazananlarListesi;
+    private JLabel Kazananlar;
+    private JButton cekilisYap;
+
+
+
+}
